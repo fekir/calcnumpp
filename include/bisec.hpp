c77931503aaa76ab51b3fa059569212dad19db29
fekir <federico.kircheis@gmail.com>
2017-08-20 13:42:31 +0200

    Add implementation of bisection algorithm

---
+++ include/bisec.hpp
@@ -0,0 +1,70 @@
+#ifndef CALCNUM_BISEC_HPP_0925487529
+#define CALCNUM_BISEC_HPP_0925487529
+
+#include "intervals.hpp"
+#include "utils.hpp"
+
+#include <functional>
+#include <cassert>
+namespace calcnum{
+
+    // length(interval) and max(|fa|,|fb|), min(|fa|,|fb|) can be used as a stopping criteria
+    struct bisect_status{
+        closed_interval interval;
+        double fa;
+        double fb;
+    };
+
+    class bisect_iter{
+        std::function<double(double)> f;
+        bisect_status st;
+
+    public:
+        bool invariant() const {
+            return (signum(st.fa)*signum(st.fb) <= 0) && calcnum::invariant(st.interval);
+        }
+
+        explicit bisect_iter(const std::function<double(double)>& f_, const closed_interval& interval) : f(f_){
+            st.interval = interval;
+            st.fa = f(interval.a);
+            st.fb = f(interval.b);
+
+            test_invariant<bisect_iter> _(*this);
+        }
+
+        bisect_iter& operator++(){
+            test_invariant<bisect_iter> _(*this);
+            const auto mid = midpoint(st.interval);
+            const auto fmid = f(mid);
+
+            const auto signfmid = signum(fmid);
+            const auto signfa = signum(st.fa);
+            const auto signfb = signum(st.fb);
+            if(signfmid == 0){ // just in case we have really hit the 0, will never happen with real data...
+                st.interval = {mid, mid};
+                st.fa = fmid;
+                st.fb = fmid;
+            } else if(signfmid*signfa == sign::greaterzero){
+                st.interval.a = mid;
+                st.fa = fmid;
+            } else if(signfmid*signfb == sign::greaterzero){
+                st.interval.b = mid;
+                st.fb = fmid;
+            } else {
+                assert(false && "FIXME: assert if some element is nan, otherwise should never happen");
+            }
+            return *this;
+        }
+
+        const bisect_status& operator*() const {
+            test_invariant<bisect_iter> _(*this);
+            return st;
+        }
+        const bisect_status* operator->() const {
+            test_invariant<bisect_iter> _(*this);
+            return &st;
+        }
+    };
+}
+
+#endif
---
+++ test/bisec.cpp
@@ -0,0 +1,58 @@
+#include <catch.hpp>
+
+#include "bisec.hpp"
+
+
+#include <functional>
+
+namespace{
+	// all test functions have a root in x = 5
+	double fun_linear(double x){
+		return x-5;
+	}
+	double fun_quad(double x){
+		return x*x-25;
+	}
+	double fun_tri(double x){
+		return x*x*x-125;
+	}
+
+	// simple test to see if the properties of the bisection algorithm are correct (intervals are at least halfed at every iteration)
+	void simple_bisec_test(const std::function<double(double)>& f, calcnum::closed_interval interval){
+		calcnum::bisect_iter it(f, interval);
+		auto oldst = *it;
+		while(length(it->interval)> 0.00001){
+			++it;
+			const auto& st = *it;
+			REQUIRE(calcnum::length(oldst.interval)/2 >= calcnum::length(st.interval));
+			oldst = st;
+		}
+	}
+}
+
+TEST_CASE("bisection"){
+	SECTION("normal case"){
+		calcnum::closed_interval interval{0,11};
+		SECTION("linear"){
+			simple_bisec_test(fun_linear, interval);
+		}
+		SECTION("quad"){
+			simple_bisec_test(fun_quad, interval);
+		}
+		SECTION("tri"){
+			simple_bisec_test(fun_tri, interval);
+		}
+	}
+	SECTION("exact root"){
+		calcnum::closed_interval interval{0,10};
+		SECTION("linear"){
+			simple_bisec_test(fun_linear, interval);
+		}
+		SECTION("quad"){
+			simple_bisec_test(fun_quad, interval);
+		}
+		SECTION("tri"){
+			simple_bisec_test(fun_tri, interval);
+		}
+	}
+}
