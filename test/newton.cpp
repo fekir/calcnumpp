c1bec7f81a4a84ddf28fa29e93bfe1a429aecc33
fekir <federico.kircheis@gmail.com>
2017-08-26 06:54:17 +0200

    Clean test cases

--- test/newton.cpp
+++ test/newton.cpp
@@ -25,30 +25,26 @@ namespace{
 		std::vector<double> err;
 
 		calcnum::newton_iter it(f, df, x0);
-		auto oldst = *it;
-		while(std::fabs(it->fx) > 0.0000000001){
+		while(std::fabs(it->fx) > 0.0000000001 && max_iter-- > 0){
+			--max_iter;
 			++it;
 			err.push_back(std::abs(sol-it->x));
-			const auto& st = *it;
-			oldst = st;
 		}
 		return err;
 	}
+
 	std::vector<double> simple_secanti_test(const std::function<double(double)>& f, double x0, double x1, double sol, std::size_t max_iter = 100){
 		std::vector<double> err;
 		calcnum::secanti_iter it(f, x0, x1);
-		auto oldst = *it;
-		while(std::fabs(it->xn - sol) > 0.0000000001){
+		while(std::fabs(it->xn - sol) > 0.0000000001 && max_iter-- > 0){
 			++it;
 			err.push_back(std::abs(sol-it->xn));
-			const auto& st = *it;
-			oldst = st;
 		}
 		return err;
 	}
 }
 
-TEST_CASE("newton"){
+TEST_CASE("newton", "[newton]"){
     SECTION("simple_fun"){
         const auto err = simple_newton_test(fun_new, dfun_new, 7, 2);
         const auto conv = calcnum::calculate_convergency(err);
@@ -56,13 +52,13 @@ TEST_CASE("newton"){
         REQUIRE_FALSE(calcnum::is_outlier(res, 2));
     }
     SECTION("complex_fun"){
-        SECTION("quad_conv"){
+        SECTION("lin_conv"){
             const auto err = simple_newton_test(fun_new_doubleroot, dfun_new_doubleroot, 0.1,0);
             const auto conv = calcnum::calculate_convergency(err);
             auto res = calcnum::analyze_data(calcnum::clear_from_inf_nan(conv));
             REQUIRE_FALSE(calcnum::is_outlier(res, 1));
         }
-        SECTION("lin_conv"){
+        SECTION("quad_conv"){
             auto err = simple_newton_test(fun_new_doubleroot, dfun_new_doubleroot, 1.2,1.27970133100099630500239591776735167562639703793577);
             const auto conv = calcnum::calculate_convergency(err);
             auto res = calcnum::analyze_data(calcnum::clear_from_inf_nan(conv));
@@ -73,7 +69,7 @@ TEST_CASE("newton"){
 
 
 
-TEST_CASE("secanti"){
+TEST_CASE("secant", "[newton][secant]"){
     SECTION("simple_fun"){
         const auto err = simple_secanti_test(fun_new, 7, 7.01, 2);
         const auto conv = calcnum::calculate_convergency(err);
@@ -82,14 +78,14 @@ TEST_CASE("secanti"){
         REQUIRE_FALSE(calcnum::is_outlier(res, 1.618));
     }
     SECTION("complex_fun"){
-        SECTION("quad_conv"){
+        SECTION("lin_conv"){
             const auto err = simple_secanti_test(fun_new_doubleroot, 0.1, 0.2, 0);
             const auto conv = calcnum::calculate_convergency(err);
             auto res = calcnum::analyze_data(calcnum::clear_from_inf_nan(conv));
             REQUIRE_FALSE(calcnum::is_outlier(res, 1));
         }
-        SECTION("lin_conv"){
-            const auto err = simple_secanti_test(fun_new_doubleroot, 1.2, 1.1, 1.27970133100099630500239591776735167562639703793577);
+        SECTION("superlin_conv"){
+            const auto err = simple_secanti_test(fun_new_doubleroot, 1.2, 1.3, 1.27970133100099630500239591776735167562639703793577);
             const auto conv = calcnum::calculate_convergency(err);
             auto res = calcnum::analyze_data(calcnum::clear_from_inf_nan(conv));
             REQUIRE_FALSE(calcnum::is_outlier(res, 1.618));
